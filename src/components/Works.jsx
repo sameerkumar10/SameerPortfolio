7e1476415e7b9c05566425e29bf40b7627e233b3
Sameer Kumar <103487647+sameerkumar10@users.noreply.github.com>
2024-07-16 21:14:35 +0530

    Changes done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-// import { github } from "../assets";
+//import { github } from "../assets";
 import { linkImage } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
@@ -19,8 +19,8 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div
-       variants={fadeIn("up", "spring", index * 0.5, 0.75)}
-       className="w-full sm:w-[280px] p-2"
+      variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+     className="w-full sm:w-[300px] p-2"
     >
       <Tilt
         options={{
@@ -28,7 +28,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl "
+        className="bg-tertiary p-5 rounded-2xl"
       >
         <div className="relative w-full aspect-video">
           <img
@@ -50,14 +50,14 @@ const ProjectCard = ({
           </div>
         </div>
         <div className="mt-5">
-          <h3 className="text-white font-bold text-[18px] sm:text-[22px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[12px] sm:text-[14px]">{description}</p>
+        <h3 className="text-white font-bold text-[20px] sm:text-[24px]">{name}</h3>
+        <p className="mt-2 text-secondary text-[12px] sm:text-[14px]">{description}</p>
         </div>
         <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
-              className={`text-[10px] sm:text-[12px] ${tag.color}`}
+              className={`text-[14px] ${tag.color}`}
             >
               #{tag.name}
             </p>
@@ -72,14 +72,14 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-secondary text-[16px] sm:text-[18px]`}>My work</p>
-        <h2 className={`${styles.sectionHeadText} text-white text-[24px] sm:text-[32px]`}>Projects.</h2>
+        <p className={`${styles.sectionSubText} text-secondary`}>My work</p>
+        <h2 className={`${styles.sectionHeadText} text-white`}>Projects.</h2>
       </motion.div>
 
       <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-       className="mt-3 text-secondary text-[14px] sm:text-[16px] max-w-3xl leading-[26px sm:-[30px]"
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcase my skills and experience through
           real-world examples of my work. Each project is briefly described with
