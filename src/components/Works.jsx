1761643a87c1cb21a20366741220e63a2977f604
Sameer Kumar <103487647+sameerkumar10@users.noreply.github.com>
2024-07-16 19:50:44 +0530

    Changes done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-//import { github } from "../assets";
+// import { github } from "../assets";
 import { linkImage } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
