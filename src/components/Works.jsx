5c443531505b56f12e9a2260fa9775e4d105f2bb
Sameer Kumar <103487647+sameerkumar10@users.noreply.github.com>
2024-07-17 15:31:21 +0530

    Changes done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -74,7 +74,6 @@ const Works = () => {
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} text-secondary`}>My work</p>
         <h2 className={`${styles.sectionHeadText} text-white`}>Projects.</h2>
-        <p className={`${styles.sectionSubText} text-secondary`}>For more Projects visit My Github</p>
       </motion.div>
 
       <div className="w-full flex">
@@ -92,7 +91,7 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className="mt-10 flex flex-wrap justify-center gap-5 sm:justify-start">
+      <div className="mt-10 flex flex-wrap gap-5 justify-start sm:justify-center">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
